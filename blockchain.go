41839bb640831f8778a90b7f0b38bdf8ea03721c
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2023-08-22 18:00:59 -0700

    creating blockchain structure

--- blockchain.go
+++ blockchain.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -30,11 +31,43 @@ func (b *Block) Print() {
 	fmt.Printf("transactions      %s\n", b.transactions)
 }
 
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+func NewBlockchain() *Blockchain {
+	bc := new(Blockchain)
+	bc.CreateBlock(0, "init hash")
+	return bc
+}
+
+func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+	return b
+}
+
+func (bc *Blockchain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d %s\n", strings.Repeat("=", 15), i,
+			strings.Repeat("=", 15))
+		block.Print()
+	}
+	fmt.Printf("%s\n", strings.Repeat("*", 15))
+}
+
 func init() {
 	log.SetPrefix("Blockchain: ")
 }
 
 func main() {
-	b := NewBlock(0, "init hash")
-	b.Print()
+	blockChain := NewBlockchain()
+	blockChain.Print()
+	blockChain.CreateBlock(1, "Initial Hash 1")
+	blockChain.Print()
+	blockChain.CreateBlock(2, "Secondary Hash 2")
+	blockChain.Print()
+	blockChain.CreateBlock(3, "Third Hash 3")
+	blockChain.Print()
 }
