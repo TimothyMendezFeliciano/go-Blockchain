28982ea81de3a13933cc1dc660663d273814d6ec
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2023-08-31 12:40:56 -0400

    Transaction UI

--- wallet_server/wallet_server.go
+++ wallet_server/wallet_server.go
@@ -1,1 +1,73 @@
 package main
+
+import (
+	"go-blockchain/utils"
+	"go-blockchain/wallet"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"path"
+	"strconv"
+)
+
+const tempDir = "wallet_server/templates"
+
+type WalletServer struct {
+	port    uint16
+	gateway string
+}
+
+func NewWalletServer(port uint16, gateway string) *WalletServer {
+	return &WalletServer{port, gateway}
+}
+
+func (ws *WalletServer) Port() uint16 {
+	return ws.port
+}
+
+func (ws *WalletServer) Gateway() string {
+	return ws.gateway
+}
+
+func (ws *WalletServer) Index(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		t, _ := template.ParseFiles(path.Join(tempDir, "index.html"))
+		t.Execute(w, "")
+
+	default:
+		log.Printf("Error: Invalid HTTP Method")
+	}
+}
+
+func (ws *WalletServer) Wallet(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodPost:
+		w.Header().Add("Content-Type", "application/json")
+		newWallet := wallet.NewWallet()
+		m, _ := newWallet.MarshalJSON()
+		io.WriteString(w, string(m[:]))
+
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println("ERROR: Invalid HTTP Method")
+	}
+}
+
+func (ws *WalletServer) CreateTransaction(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodPost:
+		io.WriteString(w, string(utils.JsonStatus("success")))
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println("Error: Invalid HTTP Method")
+	}
+}
+
+func (ws *WalletServer) Run() {
+	http.HandleFunc("/", ws.Index)
+	http.HandleFunc("/wallet", ws.Wallet)
+	http.HandleFunc("/transaction", ws.CreateTransaction)
+	log.Println(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(ws.Port())), nil))
+}
